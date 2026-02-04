294b1297032ebea2827a7004a6c5823a4b15714a
JFonseca-BC <jfonseca10@my.bcit.ca>
2026-02-03 19:10:51 -0800

    Initial commit

---
+++ lang/messages/en/html.js
@@ -0,0 +1,19 @@
+// ./lang/messages/en/html.js
+
+class HTMLText {
+    constructor() {
+        this.header = `
+        <head>
+            <title>Lab 2</title>
+            <meta charset="UTF-8">
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                }
+            </style>
+        </head>
+        `;
+    }
+}
+
+module.exports = HTMLText;
---
+++ lang/messages/en/user.js
@@ -0,0 +1,16 @@
+// ./lang/messages/en/user.js
+
+// class UserMessages {
+//     constructor() {
+//         this.baseMessage = "Hello %1, What a beautiful day. Server current date and time is ";
+//     }
+
+//     getMessage(username, dateTime) {
+//         let welcomeMessage = this.baseMessage.replace("%1", username);
+//         welcomeMessage += dateTime;
+
+//         return welcomeMessage;
+//     }
+// }
+
+// module.exports = UserMessages;
